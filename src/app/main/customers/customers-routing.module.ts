dd297a98c68f5c34b034bc3a6198a54c85fcbe2e
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-02-25 16:27:26 +0100

    Exercise 3

--- src/app/main/customers/customers-routing.module.ts
+++ src/app/main/customers/customers-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersDetailComponent } from './customers-detail/customers-detail.component';
 import { CustomersHomeComponent } from './customers-home/customers-home.component';
+import { CustomersNewComponent } from './customers-new/customers-new.component';
 
 
 const routes: Routes =  [{
   path : '',
   component: CustomersHomeComponent
+},
+{
+  path: "new",
+  component: CustomersNewComponent
+},
+{
+  path: ":CUSTOMERID",
+  component: CustomersDetailComponent
 }];
 
 @NgModule({
