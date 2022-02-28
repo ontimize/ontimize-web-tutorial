8282883c394a460cb27a19ba968c6cde47f06591
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-02-28 08:14:49 +0100

    Exercise 4

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -12,7 +12,8 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-      { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
+      { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+      { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) }
     ]
   }
 ];
