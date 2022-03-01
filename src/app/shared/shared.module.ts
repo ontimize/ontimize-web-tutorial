847400fd38bdd722f97078aab3d933e6ec350e54
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-01 17:15:07 +0100

    Exercise 14
    (cherry picked from commit e2692ec289ac2369391f129c67751b7fedfde521)
    (cherry picked from commit e2483fd3f24ce92327ed77cabc26489207b9995d)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
-import { MovementColumnRendererComponent } from '../main/accounts/accounts-detail/movement-column-renderer/movement-column-renderer.component';
+import { CommonModule } from '@angular/common';
 import { AccountNumberRenderComponent } from '../main/accounts/accounts-home/account-number-render/account-number-render.component';
 import { CustomertypeColumnRendererComponent } from '../main/customers/customers-home/customertype-column-renderer/customertype-column-renderer.component';
+import { MovementColumnRendererComponent } from '../main/accounts/accounts-detail/movement-column-renderer/movement-column-renderer.component';
+import { MENU_COMPONENTS } from './app.menu.config';
+import { starsWarsServiceFactory } from '../main/service-ex/service-ex.module';
+import { StarsWarsService } from './star-wars.service';
 
-export function intRateMonthlyFunction (rowData: Array<any>): number {
-  return rowData["INTERESRATE"]/12;
+
+export function intRateMonthlyFunction(rowData: Array<any>): number {
+  return rowData["INTERESRATE"] / 12;
 }
 
 @NgModule({
@@ -16,13 +20,21 @@ export function intRateMonthlyFunction (rowData: Array<any>): number {
   declarations: [
     AccountNumberRenderComponent,
     CustomertypeColumnRendererComponent,
-    MovementColumnRendererComponent
+    MovementColumnRendererComponent,
+    ...MENU_COMPONENTS
   ],
   exports: [
     CommonModule,
     AccountNumberRenderComponent,
     CustomertypeColumnRendererComponent,
-    MovementColumnRendererComponent
-  ]
+    MovementColumnRendererComponent,
+    ...MENU_COMPONENTS
+  ],
+  entryComponents: [...MENU_COMPONENTS],
+  providers: [{
+    provide: StarsWarsService,
+    useFactory: starsWarsServiceFactory,
+    deps: [Injector]
+  }]
 })
 export class SharedModule { }
