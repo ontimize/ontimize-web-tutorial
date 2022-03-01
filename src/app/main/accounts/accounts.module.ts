29699fc0a34d7546ea44b2e2dc7c5528c48d3a46
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-01 09:43:21 +0100

    Exercise 6

--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [AccountsHomeComponent],
   imports: [
     CommonModule,
+    SharedModule,
     OntimizeWebModule,
     AccountsRoutingModule
   ]
