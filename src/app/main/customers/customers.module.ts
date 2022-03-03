e20e26ccc8c0d4de3f358e38c019209077421332
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-03-03 11:53:01 +0100

    Exercise 18
    (cherry picked from commit f2c22d738085acb478647c1d19aeda6a92974109)
    (cherry picked from commit 4d4e8f96c132a88a71fe0e591c81d14ad896da41)
    (cherry picked from commit caccc8a13589f38b192f17ad3f607344e776745d)
    (cherry picked from commit 0f61b72ed671f62e64bf8b65bb22f6de12f6e59c)

--- src/app/main/customers/customers.module.ts
+++ src/app/main/customers/customers.module.ts
@@ -6,15 +6,19 @@ import { CustomersHomeComponent } from './customers-home/customers-home.componen
 import { CustomersDetailComponent } from './customers-detail/customers-detail.component';
 import { CustomersNewComponent } from './customers-new/customers-new.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AddAccountComponent } from './customers-detail/add-account/add-account.component';
 
 
 @NgModule({
-  declarations: [CustomersHomeComponent, CustomersDetailComponent, CustomersNewComponent],
+  declarations: [CustomersHomeComponent, CustomersDetailComponent, CustomersNewComponent, AddAccountComponent],
   imports: [
     CommonModule,
     SharedModule,
     OntimizeWebModule,
     CustomersRoutingModule
+  ],
+  entryComponents: [
+    AddAccountComponent
   ]
 })
 export class CustomersModule { }
