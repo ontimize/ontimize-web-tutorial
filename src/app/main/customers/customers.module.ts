c1486eccfd06b3a9484f00d490531d16253574c4
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-01-25 14:31:38 +0100

    Exercise 3

--- src/app/main/customers/customers.module.ts
+++ src/app/main/customers/customers.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersHomeComponent } from './customers-home/customers-home.component';
+import { CustomersDetailComponent } from './customers-detail/customers-detail.component';
+import { CustomersNewComponent } from './customers-new/customers-new.component';
 
 
 @NgModule({
   declarations: [
-    CustomersHomeComponent
+    CustomersHomeComponent,
+    CustomersDetailComponent,
+    CustomersNewComponent
   ],
   imports: [
     CommonModule,
