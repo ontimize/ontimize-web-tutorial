77944d1a440b3b343c960ad62a3b276833fd6267
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-02-08 08:59:28 +0100

    Exercise 16

--- src/app/main/customers/customers-home/customers-home.component.ts
+++ src/app/main/customers/customers-home/customers-home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OTableButtonComponent, OntimizeService } from 'ontimize-web-ngx';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-customers-home',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class CustomersHomeComponent {
 
+  public showWaitForLongTask = false;
+  private subscription: Subscription;
+
+  constructor(
+    private ontimizeService: OntimizeService
+  ) {
+    this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration("customers"));
+  }
+
+  longTaskToBackend() {
+    this.showWaitForLongTask = true;
+    this.subscription = this.ontimizeService.query(undefined, [], 'longTask').subscribe({
+      next: (res: any) => {
+        console.log("Long task finished");
+      },
+      error: (err: any) => this.showWaitForLongTask = false,
+      complete: () => this.showWaitForLongTask = false
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
